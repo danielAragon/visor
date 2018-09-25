389ee1922dd328f2758cbc105badf2792ebbd182
Daniel Aragon <daniel.aragon.0235@hotmail.com>
2018-09-25 10:01:16 -0500

    base commit

---
+++ VisorApp/VisorCore/src/test/java/pe/com/core/test/unit/suite/VisorUnitTest.java
@@ -0,0 +1,11 @@
+package pe.com.core.test.unit.suite;
+
+import org.junit.experimental.categories.Categories;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Categories.class)
+@SuiteClasses({Categories.class})
+public class VisorUnitTest {
+
+}
---
+++ VisorApp/VisorCore/src/test/java/pe/com/core/test/unit/test/ProductoTest.java
@@ -0,0 +1,5 @@
+package pe.com.core.test.unit.test;
+
+public class ProductoTest {
+
+}
